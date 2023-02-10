33cb3e78d17049ea155f8d72b0586bd825b31546
Nikita Sidukov <nsidukov@icloud.com>
2023-02-10 23:31:56 +0300

    dsadsad

---
+++ src/components/about.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="blur">
+            <div className="col-xs-12 col-md-6">
+              {" "}
+              <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            </div>
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>О нас</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>Почему вам нужно выбрать нас</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
